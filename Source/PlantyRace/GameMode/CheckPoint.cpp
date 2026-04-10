82b4ed72944edf149c406e43d3ebca9793ba4d30
ParkChanWoo <dksktlq5@gmail.com>
2026-04-10 18:26:09 +0900

    Add: 스폰포인트, 체크포인트, 리스폰트리거 추가

---
+++ Source/PlantyRace/GameMode/CheckPoint.cpp
@@ -0,0 +1,51 @@
+﻿#include "GameMode/CheckPoint.h"
+#include "Components/BoxComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/SceneComponent.h"
+
+ACheckPoint::ACheckPoint()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetupAttachment(SceneRoot);
+	BoxCollision->SetBoxExtent(FVector(100.f, 100.f, 100.f));
+	BoxCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxCollision->SetCollisionResponseToAllChannels(ECR_Ignore);
+	BoxCollision->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	ArrowComponent = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+	ArrowComponent->SetupAttachment(SceneRoot);
+}
+
+void ACheckPoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (BoxCollision)
+	{
+		BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ACheckPoint::OnCheckpointBeginOverlap);
+	}
+}
+
+void ACheckPoint::OnCheckpointBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult
+)
+{
+	if (!OtherActor)
+	{
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Checkpoint Overlap: %s entered CheckPoint %d"),
+		*OtherActor->GetName(),
+		CheckpointIndex);
+}
