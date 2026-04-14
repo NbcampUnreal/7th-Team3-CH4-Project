a63566d34c01ed75aab81d013f1f992825d1c2e1
rraw <wjdghtmd1907@naver.com>
2026-04-14 19:12:34 +0900

    펫 클래스 추가
    추가

---
+++ Source/PlantyRace/Actors/Characters/Pet/PRCarrotPet.h
@@ -0,0 +1,28 @@
+﻿// Copyright © 2026 33Fellowship. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Characters/PRPetCharacter.h"
+#include "PRCarrotPet.generated.h"
+
+UCLASS()
+class PLANTYRACE_API APRCarrotPet : public APRPetCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APRCarrotPet();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
