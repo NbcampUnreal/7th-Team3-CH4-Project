c2ec86336d1a3edb376cb253666cb614a3d29b68
voov-v <yumin5482@naver.com>
2026-04-15 19:06:29 +0900

    Add : 점프대
    점프대

---
+++ Source/PlantyRace/Actors/Traps/PRJumpPad.cpp
@@ -0,0 +1,48 @@
+﻿#include "PRJumpPad.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Actors/Characters/PlantyRaceCharacter.h"
+
+APRJumpPad::APRJumpPad()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetupAttachment(MeshComp);
+	BoxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	BoxComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	BoxComp->SetGenerateOverlapEvents(true);
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &APRJumpPad::OnBeginOverlap);
+}
+
+void APRJumpPad::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	APlantyRaceCharacter* Character = Cast<APlantyRaceCharacter>(OtherActor);
+	if (!IsValid(Character))
+	{
+		return;
+	}
+
+	const FVector LaunchVelocity = GetLaunchVelocity();
+
+	Character->LaunchCharacter(LaunchVelocity, true, true);
+}
+
+FVector APRJumpPad::GetLaunchVelocity() const
+{
+	const FVector UpVelocity = GetActorUpVector() * LaunchUpPower;
+	if (JumpMode == EJumpPadMode::Z)
+	{
+		return UpVelocity;
+	}
+
+	const FVector ForwardVelocity = GetActorForwardVector() * LaunchForwardPower;
+
+	return UpVelocity + ForwardVelocity;
+}
+
+
---
+++ Source/PlantyRace/Actors/Traps/PRJumpPad.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PRJumpPad.generated.h"
+
+class UStaticMeshComponent;
+class UBoxComponent;
+class APlantyRaceCharacter;
+
+UENUM(BlueprintType)
+enum class EJumpPadMode : uint8
+{
+	Z,
+	XZ
+};
+
+UCLASS()
+class PLANTYRACE_API APRJumpPad : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APRJumpPad();
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* BoxComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Jump")
+	EJumpPadMode JumpMode = EJumpPadMode::XZ;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Jump")
+	float LaunchUpPower = 1200.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Jump")
+	float LaunchForwardPower = 600.f;
+
+	UFUNCTION()
+	void OnBeginOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	FVector GetLaunchVelocity() const;
+};
