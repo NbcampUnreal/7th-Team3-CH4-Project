ee05eff83e2474fcaa16325634c520cdfccde528
voov-v <yumin5482@naver.com>
2026-04-15 19:07:20 +0900

    Add : 굴러다니는 돌이랑 스폰볼륨 틀정도
    굴러다니는 돌이랑 그걸 생성하는 스폰볼륨 틀 정도

---
+++ Source/PlantyRace/Actors/Traps/PRRollingActor.cpp
@@ -0,0 +1,82 @@
+﻿#include "PRRollingActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Actors/Characters/PlantyRaceCharacter.h"
+#include "Actors/Characters/Components/PRKnockbackComponent.h"
+
+APRRollingActor::APRRollingActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	MeshComp->SetNotifyRigidBodyCollision(true);
+	MeshComp->SetSimulatePhysics(true);
+	MeshComp->SetEnableGravity(true);
+	MeshComp->SetLinearDamping(0.2f);
+	MeshComp->SetAngularDamping(0.1f);
+	MeshComp->OnComponentHit.AddDynamic(this, &APRRollingActor::OnActorHit);
+}
+
+void APRRollingActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	ApplyStartImpulse();
+}
+
+void APRRollingActor::OnActorHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	if (!OtherActor || OtherActor == this)
+	{
+		return;
+	}
+
+	APlantyRaceCharacter* Character = Cast<APlantyRaceCharacter>(OtherActor);
+	if (!IsValid(Character))
+	{
+		return;
+	}
+
+	UPRKnockbackComponent* KBC = Cast<UPRKnockbackComponent>(Character->GetKnockbackComp());
+	if (!IsValid(KBC))
+	{
+		return;
+	}
+
+
+}
+
+void APRRollingActor::ApplyStartImpulse()
+{
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	FVector InitialDir = MeshComp->GetForwardVector();
+
+	float StartImpulseStrength = 0.f;
+
+}
+
+FVector APRRollingActor::GetInitialRollDirection() const
+{
+
+
+	return FVector();
+}
+
---
+++ Source/PlantyRace/Actors/Traps/PRRollingActor.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PRRollingActor.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class PLANTYRACE_API APRRollingActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APRRollingActor();
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditAnywhere, Category = "Stone")
+	float MinStartImpulse = 200.f;
+
+	UPROPERTY(EditAnywhere, Category = "Stone")
+	float MaxStartImpulse = 500.f;
+
+	UPROPERTY(EditAnywhere, Category = "Stone")
+	float KnockbackPower = 800.f;
+
+	UPROPERTY(EditAnywhere, Category = "Stone")
+	float KnockdownTime = 1.f;
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnActorHit(
+		UPrimitiveComponent* HitComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit
+	);
+
+	void ApplyStartImpulse();
+
+	FVector GetInitialRollDirection() const;
+};
