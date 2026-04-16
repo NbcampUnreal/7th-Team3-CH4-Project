75d0430b32a2a17aa01318bc043fe4dcfa613fdb
mio970407-dot <mio970407@gmail.com>
2026-04-16 13:54:39 +0900

    중간저장,타이틀용 컨트롤러

---
+++ Source/PlantyRace/Controller/PRTitlePlayerController.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright © 2026 33Fellowship. All Rights Reserved.
+
+
+#include "Controller/PRTitlePlayerController.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+void APRTitlePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsLocalController() == false)
+	{
+		return;
+	}
+
+	if (IsValid(UIWidgetClass) == true)
+	{
+		UIWidgetInstance = CreateWidget<UUserWidget>(this, UIWidgetClass);
+		if (IsValid(UIWidgetInstance) == true)
+		{
+			UIWidgetInstance->AddToViewport();
+
+			FInputModeUIOnly Mode;
+			Mode.SetWidgetToFocus(UIWidgetInstance->GetCachedWidget());
+			SetInputMode(Mode);
+
+			bShowMouseCursor = true;
+		}
+	}
+}
+
+void APRTitlePlayerController::JoinServer(const FString& InIPAddress)
+{
+	FName NextLevelName = FName(*InIPAddress);
+	UGameplayStatics::OpenLevel(GetWorld(), NextLevelName, true);
+}
