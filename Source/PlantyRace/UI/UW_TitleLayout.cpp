18676d2cd0c200d5cc54130a347c912c065eb44f
mio970407-dot <mio970407@gmail.com>
2026-04-16 14:14:25 +0900

    아마도 연결됐을것

---
+++ Source/PlantyRace/UI/UW_TitleLayout.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright © 2026 33Fellowship. All Rights Reserved.
+
+
+#include "UI/UW_TitleLayout.h"
+
+#include "Components/Button.h"
+#include "Components/EditableText.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Controller/PRTitlePlayerController.h"
+
+UUW_TitleLayout::UUW_TitleLayout(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UUW_TitleLayout::NativeConstruct()
+{
+	EnterButton.Get()->OnClicked.AddDynamic(this, &ThisClass::OnEnterButtonClicked);
+	OptionButton.Get()->OnClicked.AddDynamic(this, &ThisClass::OnOptionButtonClicked);
+	ExitButton.Get()->OnClicked.AddDynamic(this, &ThisClass::OnExitButtonClicked);
+}
+
+void UUW_TitleLayout::OnEnterButtonClicked()
+{
+	/*APRTitlePlayerController* PlayerController = GetOwningPlayer<APRTitlePlayerController>();
+	if (IsValid(PlayerController) == true)
+	{
+		FText ServerIP = ServerIPEditableText->GetText();
+		PlayerController->JoinServer(ServerIP.ToString());
+	}*/
+}
+void UUW_TitleLayout::OnOptionButtonClicked()
+{
+}
+void UUW_TitleLayout::OnExitButtonClicked()
+{
+	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, false);
+}
