14bc53e80d087ac86c69997d26c9090833a5b846
rraw <wjdghtmd1907@naver.com>
2026-04-20 17:34:29 +0900

    요청 한 것 수정완료
    LOOK은 잘 모르겠습니다
    그리고 애니메이션 잘 되게 수정했습니다.

--- Source/PlantyRace/Actors/Characters/PlantyRaceCharacter.cpp
+++ Source/PlantyRace/Actors/Characters/PlantyRaceCharacter.cpp
@@ -39,6 +39,7 @@ APlantyRaceCharacter::APlantyRaceCharacter(const FObjectInitializer& ObjectIniti
     SpringArmComp->TargetArmLength = 800.f;
     SpringArmComp->SetRelativeRotation(FRotator(-55.f, 0.f, 0.f)); // 위에서 아래로
     SpringArmComp->bUsePawnControlRotation = true;
+	SpringArmComp->ProbeChannel = ECC_Camera;
 
     CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
     CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
@@ -307,7 +308,6 @@ void APlantyRaceCharacter::ServerRelease_Implementation()
 void APlantyRaceCharacter::BeginPlay()
 {
     Super::BeginPlay();
-
     InitializeModularMeshes();
     ApplyClothesFromRepData();
 
